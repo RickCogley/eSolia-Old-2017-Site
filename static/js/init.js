7584dc2606015b116ba75618e2c6f6d299d33b9f
Rick Cogley <rick.cogley@esolia.co.jp>
2015-05-27 15:35:53 +0900

    Add js plugin functions to init.js
    Materialize doc site has this so just mimicing it

--- static/js/init.js
+++ static/js/init.js
@@ -1,8 +1,15 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    // Plugin initialization
+$('.slider').slider({full_width: true});
+$('.tab-demo').show().tabs();
+$('.parallax').parallax();
+$('.modal-trigger').leanModal();
+$('.scrollspy').scrollSpy();
+$('.button-collapse').sideNav({'edge': 'left'});
+$('.datepicker').pickadate({selectYears: 20});
+$('select').not('.disabled').material_select();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
